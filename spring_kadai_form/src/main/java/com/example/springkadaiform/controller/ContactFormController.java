2311c435781d8b3046715771f4d22af8d7514db1
yogurt-r17 <ryota.k17@gmail.com>
2025-05-03 08:24:01 +0900

    Add files via upload

--- spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -1,6 +1,5 @@
 package com.example.springkadaiform.controller;
 
-import org.springframework.core.Conventions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,21 +14,24 @@ import com.example.springkadaiform.form.ContactForm;
 public class ContactFormController {
 	@GetMapping("/form") //URLアクセス時にメソッドが呼び出される
 	public String form(Model model) { //modelにデータを格納
-		model.addAttribute("form", new ContactForm()); //フォームクラスのインスタンスを渡す
+		if (!model.containsAttribute("form")) { // リダイレクトされた時に上書きされないため
+			model.addAttribute("form", new ContactForm()); //フォームクラスのインスタンスを渡す
+		}
 		return "contactFormView";
 	}
 
-	@PostMapping("/conform")
-	public String confirm(RedirectAttributes redirectAttributes,
-			@Validated ContactForm contactForm, BindingResult result) {
+	@PostMapping("/confirm") //フォームからPOSTされたデータを受ける
+	public String confirm(RedirectAttributes redirectAttributes, //リダイレクト後に一度きりで表示されるデータを保持するためのオブジェクト
+			@Validated ContactForm form, // バリデーションを行うクラス
+			BindingResult result, Model model) { // バリデーション結果を格納するオブジェクト
 
 		if (result.hasErrors()) {
-			redirectAttributes.addFlashAttribute("contactForm", contactForm);
+			redirectAttributes.addFlashAttribute("form", form); //Flash:リダイレクト後の1回だけ使用
 
-			redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX
-					+ Conventions.getVariableName(contactForm), result);
-			return "contactFormView";
+			redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + "form", result); //バリデーション結果をビューに受け渡す
+			return "redirect:/form"; //再表示
 		}
+		model.addAttribute("form", form);
 		return "confirmView";
 
 	}
--- spring_kadai_form/src/main/java/com/example/springkadaiform/form/ContactForm.java
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/form/ContactForm.java
@@ -3,13 +3,8 @@ package com.example.springkadaiform.form;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
-@Getter
-@Setter
-
 public class ContactForm {
 	@NotBlank(message = "お名前を入力してください。")
 	private String name;
